619990b121ab9227ce509cadbaed910034ed4b72
arpan-dey-ad <arpan.dey.code@gmail.com>
2022-12-13 10:34:53 +0530

    cal.c

---
+++ cal.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    char op;
+    printf("enter a number: ");
+    scanf("%d %d",&a,&b);
+    printf("enter operator: ");
+    scanf(" %c",&op);
+    switch(op)
+    {
+    case '+':
+        c=a+b;
+        printf("%d+%d=%d",a,b,c);
+        break;
+    case '-':
+        c=a-b;
+        printf("%d-%d=%d",a,b,c);
+        break;
+    case '*':
+        c=a*b;
+        printf("%d*%d=%d",a,b,c);
+        break;
+    case '/':
+        c=a/b;
+        printf("%d/%d=%d",a,b,c);
+        break;
+    default:
+        printf("error");
+    }
+}
