c9823bc9d6491afd4a0b3245eaff46ab2c0d1763
arpan-dey-ad <arpan.dey.code@gmail.com>
2022-12-13 09:57:30 +0530

    h

---
+++ test.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("enter a number: ");
+    scanf("%d",&n);
+    {
+        while (i<=5)
+            printf("hello")
+            n++;
+    }
+}
